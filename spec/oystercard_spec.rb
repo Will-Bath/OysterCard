d703375f8370524d11d6d717ec72d159c2dac7da
jamesbingham <james.bingham@live.co.uk>
2019-10-07 15:28:01 +0100

    Challenge 5 complete

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,21 @@
-describe OysterCard do
-  
+# frozen_string_literal: true
 
+require 'oystercard'
+
+describe OysterCard do
+  it 'can check balance on card is 0' do
+    expect(subject.balance).to eq(0)
+  end
+
+  describe '#top_up' do
+    it {
+      is_expected.to respond_to(:top_up).with(1).argument # checking if top_up method has 1 variable within it
+    }
+    
+    it 'can add funds to balance' do
+      oystercard = OysterCard.new
+      oystercard.top_up(20)
+      expect(oystercard.balance).to eq(20)
+    end
+  end
 end

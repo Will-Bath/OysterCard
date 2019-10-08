5eb51369303281f8c7a127e534036133497446c3
Will <william.bath@btconnect.com>
2019-10-08 15:01:18 +0100

    Completed exercise 8

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,37 @@
 class OysterCard
 
+  attr_accessor :in_journey
   attr_reader :balance
+  MAX_BALANCE = 90
 
-  def initialize(balance = 0)
+  def initialize(balance = 0, in_journey = false)
     @balance = balance
+    @in_journey = in_journey
   end
 
   def top_up(amount)
-    @balance = amount
+    raise 'Balance will exceed limit' if @balance >= MAX_BALANCE
+    if (@balance + amount) > 90
+      raise 'Balance will exceed limit'
+    else
+      @balance += amount
+    end
+  end
+
+  def deduct(amount)
+    @balance -= amount
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
+  end
+
+  def in_journey?
+    @in_journey
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -8,14 +8,60 @@ describe OysterCard do
   end
 
   describe '#top_up' do
-    it {
-      is_expected.to respond_to(:top_up).with(1).argument # checking if top_up method has 1 variable within it
-    }
-    
+    it { is_expected.to respond_to(:top_up).with(1).argument } # checking if top_up method has 1 variable within it
+
     it 'can add funds to balance' do
-      oystercard = OysterCard.new
-      oystercard.top_up(20)
-      expect(oystercard.balance).to eq(20)
+      # oystercard = OysterCard.new
+      # oystercard.top_up(20)
+      # expect(oystercard.balance).to eq(20)
+
+      subject.top_up(20)
+      expect(subject.balance).to eq(20)
+    end
+
+    it 'prevents topping up if balance is at max balance' do
+      subject.top_up(90)
+      expect{ subject.top_up(1) }.to raise_error 'Balance will exceed limit'
+    end
+
+    it 'prevents topping up if the end balance will be above max' do
+      subject.top_up(70)
+      expect{ subject.top_up(25) }.to raise_error 'Balance will exceed limit'
+    end
+
+  end
+
+  describe '#deduct' do
+    it { is_expected.to respond_to(:deduct).with(1).argument }
+
+    it 'deducts an amount from the balance' do
+      subject.top_up(20)
+      subject.deduct(5)
+
+      expect(subject.balance).to eq(15)
     end
   end
+
+  describe '#in_journey?' do
+    it { is_expected.to respond_to(:in_journey?) }
+  end
+
+  describe 'touch_in' do
+    it { is_expected.to respond_to(:touch_in) }
+
+    it 'Changes status in_journey to true when touching in' do
+      subject.touch_in
+      expect(subject.in_journey?).to eq(true)
+    end
+  end
+
+  describe 'touch_out' do
+    it { is_expected.to respond_to(:touch_out) }
+
+    it 'Changes status in_journey to false when touching out' do
+      subject.touch_out
+      expect(subject.in_journey?).to eq(false)
+    end
+  end
+
 end
